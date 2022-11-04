bd016f26e44c28cc4ab0ae0a23321d9d1bb431ff
Umair Abidi <56173806+umairabidi@users.noreply.github.com>
2022-11-04 05:56:00 -0400

    Attempting and failing to create a simulation......

--- RotaryPendulum/RotaryPendulum.ino
+++ RotaryPendulum/RotaryPendulum.ino
@@ -1,15 +1,14 @@
-// 2022-11-03
-// Version 1
+// 2022-11-04
+// Version 2
 
 /* 
  * This code will provide the appropriate rotation to balance an inverted pendulum.
  * It does this by specifying the direction and angle of the motor.
  * The L298N Dual H-Bridge driver is used.
- * Both channels are in parallel to increase its current handling.
- * Speed is mapped from -255 to 255, with negatives being CW and positive being CCW.
+ * Speed is mapped from -255 to 255, with negatives being CW and positive being CCW. (?)
  * Angle feedback is provided by a potentiometer.
  * To begin the motion, manually move the pendulum to the vertical position and press the start button.
- * 
+ * The motor is powered with a 12 V supply for parameters Kp = 30 and Ki = 10
  * 
  */
 
@@ -20,11 +19,11 @@
 #define START_BUTTON_PIN	A4
 
 double Kp=30;
-double Ki=30;
+double Ki=10;
 double Kd=1;
-double requiredPosition;
 
-double currentPosition = 0;	//angular position
+double requiredPosition;
+double currentPosition;		//angular position
 double zeroPosition;		// The zero position that is set when the button is pressed.
 int startFlag = 0;			// Can we start the program?
 int button = 0;				// Button state
@@ -33,11 +32,11 @@ double errorInt;			// integral of error (trapezoid approximation)
 double errorDer;			// Derivative of error (secant approximation)
 double prevPosition;		// used for I and D calculations
 double calculatedMotion;	// Final output motion required for the motor
-
-
 unsigned long currentTime = 0;
 unsigned long prevTime = 0;
 unsigned long dT = 0;
+
+
 void startButton();
 void moveMotor(int Speed);
 
@@ -53,39 +52,31 @@ void setup(){
 }
 
 void loop(){
-	
 	while(!startFlag){
 		moveMotor(0);	//Brake immediately
-		startButton();
-		zeroPosition = analogRead(POT_PIN);				// Take zero as the current position
-		prevPosition = zeroPosition;
 		prevTime = 0;			// reset time
 		errorInt = 0;
 		errorDer = 0;
+		prevPosition = zeroPosition;
+		zeroPosition = analogRead(POT_PIN);				// Take zero as the current position
+		startButton();
 	}
 	
 	currentPosition = analogRead(POT_PIN);
 	currentTime = millis();
 	
 	errorPos = zeroPosition - currentPosition;
-	if (abs(errorPos) > 150){	// about 10 per 3°, 100 is 30°
+	if (abs(errorPos) > 150){	// about 10 per 3°, 150 is 45°
 		startFlag = 0;
 	}
 	
 	// PI control
 	dT = currentTime - prevTime;	
 	errorInt = dT*(0.5)*(currentPosition + prevPosition);
-	//errorDer = abs(currentPosition - prevPosition)/dT;
+	//errorDer = abs(currentPosition - prevPosition)/dT;		// This breaks the code entirely
 
-	
-//	Serial.println(errorPos);
-//	Serial.println(errorInt);
-//	Serial.println(errorDer);
-//	
 	calculatedMotion = Kp*errorPos + Ki*errorInt + Kd*errorDer;
 	moveMotor((int)calculatedMotion);
-//	Serial.print("calculatedMotion is: ");
-//	Serial.println(calculatedMotion);
 
 	prevPosition = currentPosition;
 	prevTime = currentTime;
